87b56bbb0759df7925bf5d91c7e824d1f38d4e14
manishjogi <gs.manishjogi@gmail.com>
2024-12-02 23:23:23 +0530

    initial commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appfirstQML_FILE C:/Users/gsman/OneDrive/Documents/QT/Projects/firstQML/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/appfirstQML.exe)
+set(__QT_DEPLOY_TARGET_appfirstQML_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appfirstQML_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
